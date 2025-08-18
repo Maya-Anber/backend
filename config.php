6df7d360718efb12ecf1aa5ec71cc9145e13b2ea
Maya <143032303+Maya-Anber@users.noreply.github.com>
2025-08-19 00:33:34 +0300

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// backend/_inc/config.php
-// Rename this file to config.php and set your DB credentials.
-define('DB_HOST', 'localhost');
-define('DB_USER', 'admin');
-define('DB_PASS', 'maya2004');
-define('DB_NAME', 'book_exchange');
-define('APP_ENV', 'dev'); // dev | prod
-?>
